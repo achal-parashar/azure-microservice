33ac48c905470ccdb5d36b9eaaf9cdc3c115282e
aprashar-epam <Achal_Parashar@epam.com>
2023-02-05 00:20:55 +0530

    store events processed inside rescue database

--- src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlaggedForAdoptionIntegrationEventHandler.cs
+++ src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlaggedForAdoptionIntegrationEventHandler.cs
@@ -1,10 +1,15 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using WisdomPetMedicine.Rescue.Api.Infrastructure;
+using WisdomPetMedicine.Rescue.Domain.Entities;
+using WisdomPetMedicine.Rescue.Domain.Repositories;
+using WisdomPetMedicine.Rescue.Domain.ValueObjects;
 
 namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents
 {
@@ -13,10 +18,13 @@ namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents
         private readonly ILogger<PetFlaggedForAdoptionIntegrationEventHandler> _logger;
         private readonly ServiceBusClient _client;
         private readonly ServiceBusProcessor _processor;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
         public PetFlaggedForAdoptionIntegrationEventHandler(IConfiguration configuration,
-            ILogger<PetFlaggedForAdoptionIntegrationEventHandler> logger)
+                                                            IServiceScopeFactory serviceScopeFactory,
+                                                            ILogger<PetFlaggedForAdoptionIntegrationEventHandler> logger)
         {
             _logger = logger;
+            _serviceScopeFactory = serviceScopeFactory;
             _client = new ServiceBusClient(configuration["ServiceBus:ConnectionString"]);
             _processor = _client.CreateProcessor(configuration["ServiceBus:TopicName"], configuration["ServiceBus:SubscriptionName"]);
             _processor.ProcessMessageAsync += Processor_ProcessMessageAsync;
@@ -33,7 +41,14 @@ namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents
             var body = args.Message.Body.ToString();
             var theEvent = JsonConvert.DeserializeObject<PetFlaggedForAdoptionIntegration>(body);
             await args.CompleteMessageAsync(args.Message);
-            _logger?.LogInformation(body);
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetRequiredService<IRescueRepository>();
+                var dbContext = scope.ServiceProvider.GetRequiredService<RescueDbContext>();
+                dbContext.RescuedAnimalsMetadata.Add(theEvent);
+                var rescuedAnimal = new RescuedAnimal(RescuedAnimalId.Create(theEvent.Id));
+                await repo.AddRescuedAnimalAsync(rescuedAnimal);
+            }
         }
       
         protected async override Task ExecuteAsync(CancellationToken stoppingToken)
